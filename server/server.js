64ea4d1c9f7ab5c51e6efa8b23333a593c23cb90
Kauê Rodrigues <kauefr@gmail.com>
2021-01-14 22:07:08 -0300

    Modelo Aluno alterado, Turma incluído

--- server/server.js
+++ server/server.js
@@ -10,20 +10,35 @@ app.use(bodyParser.json());
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './test.sqlite',
+    storage: ':memory:',
 });
 
 const Aluno = sequelize.define('Aluno', {
     nome: Sequelize.STRING,
     ra: Sequelize.STRING,
-    anoIngresso: Sequelize.INTEGER
+    situacao: Sequelize.STRING,
+    anoIngresso: Sequelize.INTEGER,
+    numeroChamada: Sequelize.INTEGER
 });
+
+const Turma = sequelize.define('Turma', {
+    nome: Sequelize.STRING,
+    periodo: Sequelize.STRING
+});
+
+Turma.hasMany(Aluno);
+Aluno.belongsTo(Turma);
 
 finale.initialize({ app, sequelize });
 
 finale.resource({
     model: Aluno,
-    endpoints: ['/alunos', '/alunos/:id'],
+    endpoints: ['/alunos', '/alunos/:id']
+});
+
+finale.resource({
+    model: Turma,
+    endpoints: ['/turmas', '/turmas/:id']
 });
 
 const port = 3333;
