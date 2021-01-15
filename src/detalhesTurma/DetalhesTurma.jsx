1b1b0faa26c162800f198639c0d31da6e8f8c359
Kauê Rodrigues <kauefr@gmail.com>
2021-01-15 10:37:14 -0300

    Esqueleto do componente

---
+++ src/detalhesTurma/DetalhesTurma.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+export default function DetalhesTurma(props) {
+    const { id } = useParams();
+    const [turma, setTurma] = useState();
+
+    useEffect(() => {
+        async function fetchData(id) {
+            const response = await fetch("http://localhost:3333/turmas/"+id);
+            const data = await response.json();
+            setTurma(data);
+        };
+        fetchData(id);
+    },[id]);
+    return (
+        <>
+            <h1>Detalhes da Turma</h1>
+            <p>{turma.nome}</p>
+            <p>{turma.periodo}</p>
+        </>
+    );
+}
