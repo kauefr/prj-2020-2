9faddf127f242ac9341388401fa99ff701773b45
Kauê Rodrigues <kauefr@gmail.com>
2021-01-17 22:33:05 -0300

    Lógica isolada no componente raiz da página

--- src/components/alunos/AlunoTable.jsx
+++ src/components/alunos/AlunoTable.jsx
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import AlunoRow from './AlunoRow'
 
 export default function AlunoTable(props) {
-    const [alunos, setAlunos] = useState([]);
+    const rows = props.alunos.map((aluno, index) => <AlunoRow key={index} {...aluno} />);
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch("http://localhost:3333/Alunos");
-            const data = await response.json();
-            setAlunos(data);
-        };
-        fetchData();
-    }, []);
-
-    const rows = alunos.map((aluno, index) => <AlunoRow key={index} {...aluno} />);
     return (
         <table id="contact-detail" className="table" cellSpacing="0" width="100%" >
             <thead>
