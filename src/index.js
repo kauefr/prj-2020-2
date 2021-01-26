a72a1e86a9982f6cf6879c0b35d463cafdf8f4f1
Kauê Rodrigues <kauefr@gmail.com>
2021-01-26 00:53:51 -0300

    import do CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+import 'fomantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
