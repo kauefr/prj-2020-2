0090eb9f4f4b3e2ac3232936a50a89bd62e552b8
Kauê Rodrigues <kauefr@gmail.com>
2021-01-31 21:09:05 -0300

    formatação

--- src/components/turmas/Turmas.jsx
+++ src/components/turmas/Turmas.jsx
@@ -7,37 +7,35 @@ import './turmas.css';
 export default function Turmas(props) {
     const [turmas, setTurmas] = useState([]);
     useEffect(() => {
-        getAll("Turmas",setTurmas);
+        getAll("Turmas", setTurmas);
         return;
     }, []);
 
     const rows = turmas.map((turma, index) => <TurmaRow key={index} {...turma} />);
     return (
         <>
-        <div className="container" >
+            <div className="container" >
                 <div className="options" >
-                      <Link to="/turmas/cadastrar">
-                            <button className="btn-cadastrar-turma">
-                                Cadastrar Turma
+                    <Link to="/turmas/cadastrar">
+                        <button className="btn-cadastrar-turma">
+                            Cadastrar Turma
                             </button>
-                        </Link>
-           </div>
+                    </Link>
+                </div>
 
-         <table id="turmas" className="table-turmas" cellSpacing="0" width="100%" >
-            <thead>
-                <tr>
-                    <th className="informacao-1" width="30%">Turma</th>
-                    <th className="informacao-2" width="20%">Período</th>
-                    <th className="informacao-3" width="15%">Ações</th>
-                </tr>
-            </thead>
-                <tbody>
-                   {rows}
-                </tbody>
-            </table>
-    
+                <table id="turmas" className="table-turmas" cellSpacing="0" width="100%" >
+                    <thead>
+                        <tr>
+                            <th className="informacao-1" width="30%">Turma</th>
+                            <th className="informacao-2" width="20%">Período</th>
+                            <th className="informacao-3" width="15%">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
             </div>
-                   
         </>
     );
 }
