42857f6989889415ad8f7bc6f06aee1dbfe4f892
Amanda Nalesso <nalessoamanda@github.com>
2021-02-01 17:02:05 -0300

    turmas

--- src/components/cadastrarTurma/Step2.jsx
+++ src/components/cadastrarTurma/Step2.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './CadastrarTurma.css'
+import './Step2.css'
 
 export default function Step2(props) {
 
     const rows = props.disciplinas.map((d, i) => (
     <tr key={i}>
         <td>{d.Nome}</td>
-        <td><button onClick={(e)=>{props.handleRemove(i)}}>Remover</button></td>
+        <td><button class="btn-remove" onClick={(e)=>{props.handleRemove(i)}}>Remover</button></td>
     </tr>
     ));
 
@@ -15,9 +15,9 @@ export default function Step2(props) {
             <div className="container-box">
                 <div className="wrap">
                     <div className="titulo">Adicionar Disciplinas</div>
-                    <label htmlFor="disciplina">Nome da Disciplina</label>
+                    <label classname="label" htmlFor="disciplina">Nome da Disciplina</label>
                     <input id="disciplina" value={props.current.Nome} onChange={props.handleNomeInput} />
-                    <button onClick={props.handleAddDisciplina}>Adicionar Disciplina</button>
+                    <button class="btn-add-disciplina"onClick={props.handleAddDisciplina}>Adicionar Disciplina</button>
                     <table>
                         <thead>
                             <tr>
